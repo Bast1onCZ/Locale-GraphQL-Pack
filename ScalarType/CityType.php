86991e519540dc3034714c6ad28d3bf93b63d5a8
klobaska <mir.klobaska@seznam.cz>
2020-08-09 08:32:04 +0200

    init

---
+++ ScalarType/CityType.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace BastSys\LocaleGraphQLPack\ScalarType;
+
+use BastSys\GraphQLBundle\GraphQL\ScalarType\RegExpStringType;
+use BastSys\GraphQLBundle\GraphQL\ScalarType\TDynamicName;
+
+/**
+ * Class CityType
+ * @package BastSys\GraphQLBundle\GraphQL\ScalarType\Localisation
+ * @author mirkl
+ */
+class CityType extends RegExpStringType
+{
+    use TDynamicName;
+
+    /**
+     *
+     */
+    const cityRE = '/^[\w ]+$/';
+
+    /**
+     * CityType constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::cityRE, true);
+    }
+}
